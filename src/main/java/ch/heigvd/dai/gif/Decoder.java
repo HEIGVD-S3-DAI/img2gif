9e33bec02344b34976194ee56366a3b369e22903
Leonard <73912641+leonardcser@users.noreply.github.com>
2024-10-14 14:50:22 +0200

    Add encoder + decoder

---
+++ src/main/java/ch/heigvd/dai/gif/Decoder.java
@@ -0,0 +1,41 @@
+package ch.heigvd.dai.gif;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+
+public class Decoder {
+
+  /**
+   * Decodes a GIF byte array into a list of BufferedImage objects.
+   *
+   * @param gifData Byte array representing the GIF data.
+   * @return List of BufferedImage objects extracted from the GIF.
+   * @throws IOException If an I/O error occurs during decoding.
+   */
+  public List<BufferedImage> decodeGif(byte[] gifData) throws IOException {
+    List<BufferedImage> images = new ArrayList<>();
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(gifData);
+    ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream);
+    Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("gif");
+
+    if (readers.hasNext()) {
+      ImageReader reader = readers.next();
+      reader.setInput(imageInputStream);
+
+      int numFrames = reader.getNumImages(true);
+      for (int i = 0; i < numFrames; i++) {
+        BufferedImage image = reader.read(i);
+        images.add(image);
+      }
+    }
+
+    return images;
+  }
+}
---
+++ src/main/java/ch/heigvd/dai/gif/Encoder.java
@@ -0,0 +1,123 @@
+package ch.heigvd.dai.gif;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.imageio.stream.ImageOutputStream;
+
+public class Encoder {
+
+  /**
+   * Encodes a list of BufferedImage objects into a GIF byte array.
+   *
+   * @param images List of BufferedImage objects to be encoded.
+   * @param loopCount Number of times the GIF should loop. 0 means infinite loop.
+   * @param delay Delay time between frames in milliseconds.
+   * @return Byte array representing the encoded GIF.
+   * @throws IOException If an I/O error occurs during encoding.
+   */
+  public byte[] encodeGif(List<BufferedImage> images, int loopCount, int delay) throws IOException {
+    if (images == null || images.isEmpty()) {
+      throw new IllegalArgumentException("Image list cannot be null or empty");
+    }
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(outputStream);
+    Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("gif");
+
+    if (writers.hasNext()) {
+      ImageWriter writer = writers.next();
+      writer.setOutput(imageOutputStream);
+      writer.prepareWriteSequence(null);
+
+      IIOMetadata metadata = createMetadata(writer, images.get(0), loopCount, delay);
+
+      for (BufferedImage image : images) {
+        writer.writeToSequence(new javax.imageio.IIOImage(image, null, metadata), null);
+      }
+
+      writer.endWriteSequence();
+      imageOutputStream.close();
+    }
+
+    return outputStream.toByteArray();
+  }
+
+  /**
+   * Creates and configures the metadata for the GIF.
+   *
+   * @param writer ImageWriter used to write the GIF.
+   * @param firstImage The first image in the list to get the image type specifier.
+   * @param loopCount Number of times the GIF should loop.
+   * @param delay Delay time between frames in milliseconds.
+   * @return Configured IIOMetadata object.
+   * @throws IOException If an I/O error occurs during metadata creation.
+   */
+  private IIOMetadata createMetadata(
+      ImageWriter writer, BufferedImage firstImage, int loopCount, int delay) throws IOException {
+    IIOMetadata metadata =
+        writer.getDefaultImageMetadata(new javax.imageio.ImageTypeSpecifier(firstImage), null);
+    String metaFormatName = metadata.getNativeMetadataFormatName();
+    IIOMetadataNode root = (IIOMetadataNode) metadata.getAsTree(metaFormatName);
+
+    configureGraphicsControlExtension(root, delay);
+    configureApplicationExtensions(root, loopCount);
+
+    metadata.setFromTree(metaFormatName, root);
+    return metadata;
+  }
+
+  /**
+   * Configures the Graphics Control Extension node with the delay time.
+   *
+   * @param root Root metadata node.
+   * @param delay Delay time between frames in milliseconds.
+   */
+  private void configureGraphicsControlExtension(IIOMetadataNode root, int delay) {
+    IIOMetadataNode graphicsControlExtensionNode = getNode(root, "GraphicControlExtension");
+    graphicsControlExtensionNode.setAttribute("delayTime", Integer.toString(delay / 10));
+    graphicsControlExtensionNode.setAttribute("disposalMethod", "restoreToBackgroundColor");
+  }
+
+  /**
+   * Configures the Application Extensions node with the loop count.
+   *
+   * @param root Root metadata node.
+   * @param loopCount Number of times the GIF should loop.
+   */
+  private void configureApplicationExtensions(IIOMetadataNode root, int loopCount) {
+    IIOMetadataNode applicationExtensionsNode = getNode(root, "ApplicationExtensions");
+    IIOMetadataNode applicationExtensionNode = new IIOMetadataNode("ApplicationExtension");
+    applicationExtensionNode.setAttribute("applicationID", "NETSCAPE");
+    applicationExtensionNode.setAttribute("authenticationCode", "2.0");
+
+    byte[] loopBytes = new byte[] {1, (byte) (loopCount & 0xFF), (byte) ((loopCount >> 8) & 0xFF)};
+    applicationExtensionNode.setUserObject(loopBytes);
+    applicationExtensionsNode.appendChild(applicationExtensionNode);
+  }
+
+  /**
+   * Retrieves or creates a metadata node with the specified name.
+   *
+   * @param rootNode Root metadata node.
+   * @param nodeName Name of the node to retrieve or create.
+   * @return The retrieved or newly created IIOMetadataNode.
+   */
+  private IIOMetadataNode getNode(IIOMetadataNode rootNode, String nodeName) {
+    int nNodes = rootNode.getLength();
+    for (int i = 0; i < nNodes; i++) {
+      if (rootNode.item(i).getNodeName().equalsIgnoreCase(nodeName)) {
+        return (IIOMetadataNode) rootNode.item(i);
+      }
+    }
+    IIOMetadataNode node = new IIOMetadataNode(nodeName);
+    rootNode.appendChild(node);
+    return node;
+  }
+}
