7bb100ba95e45efabb14bbfbfda425d2eec939a8
Aude Laydu <audel1814@gmail.com>
2024-10-17 16:26:07 +0200

    first version

---
+++ src/main/java/ch/heigvd/dai/gif/IoDecode.java
@@ -0,0 +1,45 @@
+package ch.heigvd.dai.gif;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+import javax.imageio.*;
+
+public class IoDecode {
+
+    /**
+     * Read a Gif file and convert it to a byteArray.
+     * @param fileName the name of the file to read.
+     * @return a byte array corresponding to the file's content.
+     */
+    public static byte[] readGif(String fileName) {
+        byte[] data = null;
+        try (FileInputStream fis = new FileInputStream(fileName);
+            BufferedInputStream bis = new BufferedInputStream(fis))
+        {
+            data = bis.readAllBytes();
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return data;
+    }
+
+    /**
+     * Create and write BufferedImage in output files in a chosen directory.
+     * @param directoryName the name of the directory to save the files in.
+     * @param images a List of BufferedImage to save as jpg files.
+     */
+    public void writeImages(String directoryName, List<BufferedImage> images) {
+        for (int i = 1; i <= images.size(); i++) {
+            try(FileOutputStream fos = new FileOutputStream(directoryName + "/image_" + i);
+                BufferedOutputStream bos = new BufferedOutputStream(fos))
+            {
+                ImageIO.write(images.get(i - 1), "jpg", bos);
+                bos.flush();
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+    }
+}
