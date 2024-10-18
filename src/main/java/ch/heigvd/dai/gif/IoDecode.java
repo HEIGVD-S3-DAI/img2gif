a6f62ec58655dd2563c8d039ebe128d9bb1a9b6e
eau2 <94754849+eau2@users.noreply.github.com>
2024-10-18 13:49:08 +0200

    3 create gifio class (#6)
    * first version
    
    * update project configuration
    
    * Fixed paths construction to acess images inside a directory  correctly using java.nio.file.Paths
    
    * Fix code and javadocs formatting.

---
+++ src/main/java/ch/heigvd/dai/gif/IoDecode.java
@@ -0,0 +1,46 @@
+package ch.heigvd.dai.gif;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+import javax.imageio.*;
+
+public class IoDecode {
+
+  /**
+   * Read a Gif file and convert it to a byteArray.
+   *
+   * @param fileName the name of the file to read.
+   * @return a byte array corresponding to the file's content.
+   */
+  public static byte[] readGif(String fileName) {
+    byte[] data = null;
+    try (FileInputStream fis = new FileInputStream(fileName);
+        BufferedInputStream bis = new BufferedInputStream(fis)) {
+      data = bis.readAllBytes();
+    } catch (IOException e) {
+      System.err.println("Error: " + e.getMessage());
+    }
+    return data;
+  }
+
+  /**
+   * Create and write BufferedImage in output files in a chosen directory.
+   *
+   * @param directoryName the name of the directory to save the files in.
+   * @param images a List of BufferedImage to save as jpg files.
+   */
+  public static void writeImages(String directoryName, List<BufferedImage> images) {
+    for (int i = 1; i <= images.size(); i++) {
+      try (FileOutputStream fos =
+              new FileOutputStream(Paths.get(directoryName, "image_" + i + ".png").toString());
+          BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+        ImageIO.write(images.get(i - 1), "png", bos);
+        bos.flush();
+      } catch (IOException e) {
+        System.err.println("Error: " + e.getMessage());
+      }
+    }
+  }
+}
---
+++ src/main/java/ch/heigvd/dai/gif/IoEncode.java
@@ -0,0 +1,111 @@
+package ch.heigvd.dai.gif;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+import javax.imageio.*;
+
+public class IoEncode {
+
+  /**
+   * Check if a file is an image based on its extension.
+   *
+   * @param fileName the name of the file we want to test.
+   * @return true if the file is an image, false if it isn't.
+   */
+  public static boolean isImageFile(String fileName) {
+    // Check for common image file extensions
+    return fileName.endsWith(".jpg")
+        || fileName.endsWith(".jpeg")
+        || fileName.endsWith(".png")
+        || fileName.endsWith(".bmp")
+        || fileName.endsWith(".tiff");
+  }
+
+  /**
+   * Select only the files that are images inside a directory.
+   *
+   * @param directoryName the name of the directory.
+   * @return A List of String containing names of the files that are images in the directory.
+   */
+  public static List<String> getImagesFilesNames(String directoryName) {
+    List<String> fileNames = new ArrayList<>();
+    File directory = new File(directoryName);
+
+    // Check if the directory exists and is indeed a directory
+    if (!directory.exists()) {
+      System.err.println("Directory does not exist: " + directoryName);
+      return fileNames; // Return an empty list
+    }
+
+    if (!directory.isDirectory()) {
+      System.err.println(directoryName + " is not a directory.");
+      return fileNames; // Return an empty list
+    }
+
+    // Get all the files in the directory
+    File[] files = directory.listFiles();
+
+    if (files != null) {
+      for (File file : files) {
+        // Add only file names (excluding directories)
+        if (file.isFile() && isImageFile(file.getName())) {
+          fileNames.add(Paths.get(directoryName, file.getName()).toString());
+        }
+      }
+    } else {
+      System.err.println("An error occurred while reading the directory.");
+    }
+
+    return fileNames;
+  }
+
+  /**
+   * Read a binary file and convert it to a BufferedImage.
+   *
+   * @param fileName the name of the file.
+   * @return a BufferedImage corresponding to the file read.
+   */
+  public static BufferedImage readImage(String fileName) {
+    try (InputStream fis = new FileInputStream(fileName);
+        BufferedInputStream bis = new BufferedInputStream(fis); ) {
+      return ImageIO.read(bis);
+
+    } catch (IOException e) {
+      System.err.println("Error: " + e.getMessage());
+    }
+    return null;
+  }
+
+  /**
+   * Read all binary files that are images in a directory and convert them to BufferedImage.
+   *
+   * @param directoryName the name of the directory containing the images.
+   * @return a List of BufferedImage corresponding to the image files in the directory.
+   */
+  public static List<BufferedImage> readImages(String directoryName) {
+    List<String> fileNames = getImagesFilesNames(directoryName);
+    List<BufferedImage> images = new ArrayList<>();
+    for (String fileName : fileNames) {
+      images.add(readImage(fileName));
+    }
+    return images;
+  }
+
+  /**
+   * Create a file and write the content of a byteArray in it. Used to save the output.
+   *
+   * @param outputPath path of the directory where the output file will be created.
+   * @param byteArray content that will be written in the output binary file.
+   */
+  public static void writeGif(String outputPath, byte[] byteArray) {
+    try (FileOutputStream fos = new FileOutputStream(outputPath);
+        BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+      bos.write(byteArray);
+      bos.flush();
+    } catch (IOException e) {
+      System.err.println("Error: " + e.getMessage());
+    }
+  }
+}
